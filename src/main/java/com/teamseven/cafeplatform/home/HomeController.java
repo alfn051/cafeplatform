691a230a12cf1cf3027bfa23ee1ff5a022317631
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2023-12-15 12:20:53 +0900

    homecontroller 변경

--- src/main/java/com/teamseven/cafeplatform/home/HomeController.java
+++ src/main/java/com/teamseven/cafeplatform/home/HomeController.java
@@ -1,13 +1,29 @@
 package com.teamseven.cafeplatform.home;
 
+import com.teamseven.cafeplatform.domain.user.entity.User;
+import com.teamseven.cafeplatform.domain.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final UserService userService;
+
     @GetMapping("/")
-    public String Home(){
+    public String Home(@SessionAttribute(name = "loginUser", required = false)User loginUser, HttpServletRequest httpServletRequest , Model model){
+
+        System.out.println("!!!"+loginUser);
+        if(loginUser!=null){
+            model.addAttribute("loginUser", loginUser);
+        }
+
         return "/home/index";
     }
 }
