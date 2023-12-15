82c6c04fe5b07f7b3de13b679f1dc2082035dc5c
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2023-12-15 17:00:15 +0900

    controller 기능 추가 및 수정

--- src/main/java/com/teamseven/cafeplatform/home/HomeController.java
+++ src/main/java/com/teamseven/cafeplatform/home/HomeController.java
@@ -1,29 +1,51 @@
 package com.teamseven.cafeplatform.home;
 
+import com.teamseven.cafeplatform.domain.cafe.dto.CafeDisplayDTO;
+import com.teamseven.cafeplatform.domain.cafe.entity.Cafe;
+import com.teamseven.cafeplatform.domain.cafe.service.CafeService;
 import com.teamseven.cafeplatform.domain.user.entity.User;
 import com.teamseven.cafeplatform.domain.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.util.List;
+
 @Controller
+@Slf4j
 @RequiredArgsConstructor
 public class HomeController {
 
     private final UserService userService;
 
+    private final CafeService cafeService;
+
     @GetMapping("/")
     public String Home(@SessionAttribute(name = "loginUser", required = false)User loginUser, HttpServletRequest httpServletRequest , Model model){
 
-        System.out.println("!!!"+loginUser);
+        List<Cafe> cafeList = cafeService.getAllCafe();
+        System.out.println(cafeList);
+        model.addAttribute("cafeList", cafeList);
+
+        log.info("loginUser : {}", loginUser);
+
         if(loginUser!=null){
             model.addAttribute("loginUser", loginUser);
         }
 
         return "/home/index";
+
+//    @GetMapping("/list")
+//    public String Test(Model model){
+//        List<Cafe> cafeList = cafeService.getAllCafe();
+//        System.out.println(cafeList);
+//        model.addAttribute("cafeList", cafeList);
+//
+//        return "/home/cafeList";
     }
 }
